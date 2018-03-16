eefcb81841a01d0367b0e2f5bf9284b365a69e8a
vibakar <vibakar23@gmail.com>
2018-03-16 19:55:18 +0530

    learned to access variable from another file

---
+++ 02_Packages/main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vibakar/GoLang_Learning/02_Packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Name)
+}
---
+++ 03_Variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var name string = "viba"
+var c int
+var d int = 10
+
+func main() {
+	a := 8
+	fmt.Println(name, a, c, d)
+}
